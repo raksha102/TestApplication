e3b5801370a4fe476ae7d36dc043cd4afaa49d95
raksha <raksha.shetty.102@gmail.com>
2018-07-28 11:09:17 +0530

    - initial project setup - project package structure created

---
+++ app/src/main/java/com/application/testapplication/application/MIVIApplication.java
@@ -0,0 +1,11 @@
+package com.application.testapplication.application;
+
+import android.app.Application;
+
+public class MIVIApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
